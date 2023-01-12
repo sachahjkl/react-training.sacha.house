1cd7dcd6635a631ce13ea9e8de24b8a042f1a637
Sacha FROMENT <sacha@sacha.house>
2023-01-12 12:14:04 +0100

    typo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,8 +5,8 @@ export default function IndexPage() {
         <h2>🚧 React-Training</h2>
         <p>
           This is a demo project using <b>Vite</b>, <b>Typescript</b>,{' '}
-          <b>TailwindCSS</b>, <b>React Router V6</b>, <b>Eslint+Prettier</b> and{' '}
-          <b>PNPM</b>.
+          <b>TailwindCSS</b>, <b>React Router V6</b>, <b>Eslint</b> +{' '}
+          <b>Prettier</b> and <b>PNPM</b>.
         </p>
         <p>
           I put my <b> demo components</b>, <b>training exercises</b> and
