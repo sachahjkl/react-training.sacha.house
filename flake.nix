12eaa6001a8c6ad8a50c9fa6f8f38abad7c35a47
sachahjkl <sacha@sacha.house>
2026-09-11 21:59:54 +0200

    Use the Nix community cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "react-training.sacha.house static website";
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2605";
     flake-utils.url = "github:numtide/flake-utils";
