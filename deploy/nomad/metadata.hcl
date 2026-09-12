a227da906116b3dd4d0cad7854b5562ef142c01d
sachahjkl <sacha@sacha.house>
2026-09-12 17:23:42 +0200

    Adopt the homelab application contract

---
+++ deploy/nomad/metadata.hcl
@@ -0,0 +1,9 @@
+app {
+  url = "https://github.com/sachahjkl/react-training.sacha.house"
+}
+
+pack {
+  name        = "homelab-application"
+  description = "Deploy react-training-sacha-house through Nomad and Traefik"
+  version     = "1.0.0"
+}
--- flake.nix
+++ flake.nix
@@ -103,10 +103,26 @@
             trim-trailing-whitespace.enable = true;
           };
         };
-        nomadJobs = pkgs.runCommand "${pname}-nomad-jobs" {nativeBuildInputs = [pkgs.nomad];} ''
+        nomadJobs = pkgs.runCommand "react-training-sacha-house-nomad-jobs" {nativeBuildInputs = [pkgs.nomad pkgs.nomad-pack];} ''
+          export HOME="$TMPDIR"
           image="ghcr.io/sachahjkl/react-training.sacha.house@sha256:0000000000000000000000000000000000000000000000000000000000000000"
-          nomad job validate -var "image=$image" ${./deploy/nomad/staging.nomad.hcl}
-          nomad job validate -var "image=$image" ${./deploy/nomad/production.nomad.hcl}
+          for environment in staging production; do
+            cat > "$TMPDIR/$environment.vars.hcl" <<EOFVARS
+          name = "react-training-sacha-house"
+          domain = "example.sacha.house"
+          environment = "$environment"
+          health_path = "/"
+          image = "$image"
+          port = 3000
+          service_tags = []
+          volume_enabled = false
+          volume_mount_path = ""
+          volume_name = ""
+          EOFVARS
+            nomad-pack render ${./deploy/nomad} --var-file "$TMPDIR/$environment.vars.hcl" \
+              --to-dir "$TMPDIR/$environment" --auto-approve >/dev/null
+            nomad job validate "$TMPDIR/$environment/homelab-application/application.nomad"
+          done
           touch "$out"
         '';
       in {
